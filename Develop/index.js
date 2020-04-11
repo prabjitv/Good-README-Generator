e50b79252bc5bce15350f4045cf5316f70514678
prab <58503954+prabjitv@users.noreply.github.com>
2020-04-10 17:33:18 -0700

    starting work on qustionaire

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,29 @@ const inquirer = require("inquirer");
 const dotenv = require("dotenv");
 
 const questions = [
+  inquirer
+    .prompt([
+      {
+        type: input,
+        name: username,
+        message: "Hello! What's your GitHub username?"
 
+      }
+
+
+
+
+    ])
+    .then(answers => {
+      // Use user feedback for... whatever!!
+    })
+    .catch(error => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else when wrong
+      }
+    });
 ];
 
 function writeToFile(fileName, data) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,18 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+* At least one badge
+* Project title
+* Description
+* Table of Contents
+* Installation
+* Usage
+* License
+* Contributing
+* Tests
+* Questions
+  * User GitHub profile picture
+  * User GitHub email
 `;
 }
 
